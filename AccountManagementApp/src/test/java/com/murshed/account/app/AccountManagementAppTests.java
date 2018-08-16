1ff50167fcbfe9a281fb71a7f0af7ff736a76cdb
Murshed <Murshed@Murshed-Work>
2018-08-16 16:54:48 +0100

    Initialise SpringBoot Project

---
+++ AccountManagementApp/src/test/java/com/murshed/account/app/AccountManagementAppTests.java
@@ -0,0 +1,16 @@
+package com.murshed.account.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountManagementAppTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
