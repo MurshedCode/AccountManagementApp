733140be662f0e638aa8220a5a55c40d957a21ad
Murshed <Murshed@Murshed-Work>
2018-08-17 13:42:37 +0100

    Controller test cases implemented

--- AccountManagementApp/src/test/java/com/murshed/account/app/AccountManagementControllerTest.java
+++ AccountManagementApp/src/test/java/com/murshed/account/app/AccountManagementControllerTest.java
@@ -1,16 +1,30 @@
 package com.murshed.account.app;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import org.junit.Before;
+import java.util.Optional;
+import java.util.function.IntPredicate;
+
+import org.apache.tomcat.util.http.parser.MediaType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.murshed.account.app.controller.AccountManagementController;
 import com.murshed.account.app.entities.Account;
 import com.murshed.account.app.service.AccountManagementService;
@@ -19,16 +33,19 @@ import com.murshed.account.app.service.AccountManagementService;
 @WebMvcTest( value = AccountManagementController.class, secure = false)
 public class AccountManagementControllerTest {
 
+	
+
 	@Autowired
 	private MockMvc mockMvc;
 	
 	@MockBean
 	private AccountManagementService accountManagementService;
+
 	
 	
 	
 	@Test
-	public void testaddAccounts() throws Exception {
+	public void testaddAccount() throws Exception {
 		
 		Account mockAccount = new Account();
 		mockAccount.setAccountId(1);
@@ -37,28 +54,68 @@ public class AccountManagementControllerTest {
 		mockAccount.setAccountNumber(1234);
 		
 		
+		String inpuInJson = this.mapToJson(mockAccount);
+		String URI = " /api/accounts/create";
 		
+		Mockito.when(accountManagementService.addAccount(Mockito.any(Account.class))).thenReturn(mockAccount);
 		
-		fail("Not yet implemented");
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.post(URI)
+				.accept(MediaType.APPLICATION_JSON).content(inpuInJson)
+				.contentType(MediaType.APPLICATION_JSON);
+				
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		
+		String outputInJson = response.getContentAsString();
+		
+		assertThat(outputInJson).isEqualTo(inpuInJson);
+		assertEquals(HttpStatus.OK.value(), response.getStatus());
+		
 	}
 	
 	
-	
 	@Test
-	public void testAddAccount() {
-		fail("Not yet implemented");
+	public void testGetAccountById() throws Exception {
+		
+		Account mockAccount = new  Account();
+		mockAccount.setAccountId(1);
+		mockAccount.setFirstName("John");
+		mockAccount.setSecondName("Smith");
+		mockAccount.setAccountNumber(1234);
+		
+		Mockito.when(accountManagementService.getAccountById(Mockito.anyInt())).thenReturn(mockAccount);
+		
+		String URI = "/api/accounts/account/accountId/1";
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(URI)
+				.accept(MediaType.APPLICATION_JSON);
+	
+	
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		String espectedJson = this.mapToJson(mockAccount);
+		String outputInJson = result.getResponse().getContentAsString();
+		
+		assertThat(outputInJson).isEqualTo(espectedJson);
+		
 	}
 
-	@Test
-	public void testGetAccountById() {
-		fail("Not yet implemented");
-	}
-
+	
+	
+	
+	
+	
+	
 	
 
-	@Test
-	public void testDeleteAccount() {
-		fail("Not yet implemented");
+	/**
+	 * Maps an object into JSON string   * @param object
+	 * 
+	 */
+	
+	private String mapToJson(Object object ) throws JsonProcessingException	{
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(object);
 	}
 
 	
